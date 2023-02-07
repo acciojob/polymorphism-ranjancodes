c9e397d83da392d8909e6e6b3fbbe28a7717b3a4
Rishav Ranjan <97038318+ranjancodes@users.noreply.github.com>
2023-02-08 02:39:29 +0530

    second attempt

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -3,23 +3,23 @@ package com.driver;
 public class Main {
     public static class Product{
         public int product(int x, int y) {
-            return -1;
+            return 0;
         }
         public int product(int x, int y, int z) {
             return 0;
         }
 
         public double product(double x, double y) {
-            return 1.10;
+            return 0;
         }
 
     }
 
     public static void main(String[] args) {
         Product p=new Product();
-        p.product(0,1);
-        p.product(0,1,2);
-        p.product(2.1,2.2);
+        p.product(0,0);
+        p.product(0,0,0);
+        p.product(0.0,0.0);
 
     }
 
