53417e1d5939c584301b14fe8847c8ade1606966
Rishav Ranjan <97038318+ranjancodes@users.noreply.github.com>
2023-02-08 02:31:43 +0530

    trial

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,26 @@
 package com.driver;
 
 public class Main {
+    public static class Product{
+        public int product(int x, int y) {
+            return -1;
+        }
+        public int product(int x, int y, int z) {
+            return 0;
+        }
+
+        public double product(double x, double y) {
+            return 1.10;
+        }
+
+    }
+
+    public static void main(String[] args) {
+        Product p=new Product();
+        p.product(0,1);
+        p.product(0,1,2);
+        p.product(2.1,2.2);
+
+    }
 
 }
